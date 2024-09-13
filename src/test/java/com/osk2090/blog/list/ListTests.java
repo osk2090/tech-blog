77355ffb5840dfc67fd713fdfbab1162056a6043
osk2090 <osk2090@naver.com>
2024-09-13 23:31:29 +0900

    feat(list): ArrayList 테스트 추가 및 기능 구현
    ArrayList 테스트 코드 추가 및 다양한 기능을 구현하여 ArrayList의 동작 방식을 이해하고 활용 방법을 익힙니다.
    - subList() 메서드를 활용하여 ArrayList의 일부를 복사합니다.
    - Collections.sort() 메서드를 사용하여 ArrayList를 정렬합니다.
    - containsAll() 메서드를 사용하여 ArrayList가 다른 ArrayList의 모든 요소를 포함하는지 확인합니다.
    - add(index, element) 메서드를 사용하여 특정 인덱스에 요소를 추가합니다.
    - retainAll() 메서드를 사용하여 ArrayList에 다른 ArrayList와 겹치는 요소만 남깁니다.
    - 반복문을 사용하여 ArrayList에서 특정 요소를 제거합니다.

--- src/test/java/com/osk2090/blog/list/ListTests.java
+++ src/test/java/com/osk2090/blog/list/ListTests.java
@@ -1,13 +1,16 @@
 package com.osk2090.blog.list;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
 @SpringBootTest
+@Slf4j
 public class ListTests {
 
     @Test
@@ -105,5 +108,63 @@ public class ListTests {
         // 결론 삽입하는건 둘다 결국엔 맨 마지막에 넣는거라 상관없지만
         // 특정 인덱스에 접근하는건 어레이리스트는 인덱스로 접근하기때문에 빠르고
         // 링크드리스트는 순차적으로 접근해야되기때문에 느리다.
+    }
+
+    @Test
+    void arrayListTest() {
+
+        // hint: 이건 인터페이스 타입이 아닌 구체 클래스를 타입으로 선언하여 좀더 static하여 유연한 코딩은 아니다.
+        ArrayList list1 = new ArrayList<>(10);
+
+        list1.add(5);
+        list1.add(4);
+        list1.add(2);
+        list1.add(0);
+        list1.add(1);
+        list1.add(3);
+
+        // list1 컬렉션에서 1번 인덱스부터 4번 인덱스까지 값 복사
+        ArrayList list2 = new ArrayList<>(list1.subList(1, 4));
+        log.info("일반 출력");
+        log.info("list 1: {}", list1);
+        log.info("list 2: {}", list2);
+
+        // 오름차순 정렬
+        Collections.sort(list1);
+        Collections.sort(list2);
+        log.info("정렬");
+        log.info("list 1: {}", list1);
+        log.info("list 2: {}", list2);
+
+        // list1 기준으로 list2에 있는 값을 모두 가지고 있으면 true 리턴
+        log.info("list1.containsAll(list2): {}", list1.containsAll(list2));
+
+        list2.add("B");
+        list2.add("C");
+
+        log.info("3번 인덱스에 A 저장");
+        list2.add(3, "A");
+        log.info("list 1: {}", list1);
+        log.info("list 2: {}", list2);
+
+        log.info("3번 인덱스에 AA 저장");
+        list2.add(3, "AA");
+        log.info("list 1: {}", list1);
+        log.info("list 2: {}", list2);
+
+        // list1에서 list2와 겹치는 부분만 남기고 나머지는 삭제한다.
+        log.info("list1.retainAll(list2): {}", list1.retainAll(list2));
+        log.info("list 1: {}", list1);
+        log.info("list 2: {}", list2);
+
+        // list1를 기준으로 list2에 있는 값 삭제
+        // 이렇게 뒤에서부터 확인해서 삭제한 이유는 앞에서부터 삭제하면 자리이동이 있기때문에 불필요한 자리이동 하지않음
+        for (int i = list2.size() - 1; i >= 0; i--) {
+            if (list1.contains(list2.get(i))) {
+                list2.remove(i);
+            }
+        }
+        log.info("list 1: {}", list1);
+        log.info("list 2: {}", list2);
     }
 }
