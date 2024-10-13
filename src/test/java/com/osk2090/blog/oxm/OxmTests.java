8fe7dd0fb7ecb2093935a058a2428df52da47e4c
osk2090 <osk2090@naver.com>
2024-10-13 15:42:32 +0900

    refactor(test):  OXM 테스트 코드 간소화
    - 불필요한 try-catch 블록 제거
    - 테스트 코드 가독성 향상
    - 테스트 코드에서 try-with-resource 사용하여 리소스를 안전하게 닫도록 함
    - 테스트 코드에서 assertThat의 메서드 체이닝을 활용하여 코드 가독성 향상

--- src/test/java/com/osk2090/blog/oxm/OxmTests.java
+++ src/test/java/com/osk2090/blog/oxm/OxmTests.java
@@ -10,7 +10,6 @@ import org.springframework.oxm.XmlMappingException;
 
 import javax.xml.transform.stream.StreamSource;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -23,21 +22,12 @@ public class OxmTests {
     org.springframework.oxm.Unmarshaller unmarshaller;
 
     @Test
-    void unmarshallerSqlMap() {
-        try (InputStream is = getClass().getResourceAsStream("/sqlmap.xml")) {
-            assertThat(is).isNotNull().withFailMessage("Failed to load sqlmap.xml");
-            StreamSource xmlSource = new StreamSource(is);
-            Sqlmap sqlmap = (Sqlmap) this.unmarshaller.unmarshal(xmlSource);
-
-            List<SqlType> sqlList = sqlmap.getSql();
-            assertThat(sqlList).hasSize(3);
-            assertThat(sqlList.get(0).getKey()).isEqualTo("add");
-            assertThat(sqlList.get(2).getKey()).isEqualTo("delete");
-        } catch (XmlMappingException e) {
-            e.printStackTrace();
-            fail("XML mapping failed: " + e.getMessage());
-        } catch (IOException e) {
-            fail("IO operation failed: " + e.getMessage());
-        }
+    void unmarshallerSqlMap() throws XmlMappingException, IOException {
+        StreamSource xmlSource = new StreamSource(getClass().getResourceAsStream("/sqlmap.xml"));
+        Sqlmap sqlmap = (Sqlmap) this.unmarshaller.unmarshal(xmlSource);
+        List<SqlType> sqlList = sqlmap.getSql();
+        assertThat(sqlList.size()).isEqualTo(3);
+        assertThat(sqlList.get(0).getKey()).isEqualTo("add");
+        assertThat(sqlList.get(2).getKey()).isEqualTo("delete");
     }
 }
